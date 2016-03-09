f7e3abdea70b28fc523d381c74c590efdbfaf4e5
jasonxiao <jasonxiao@tencent.com>
2016-03-09 16:05:51 +0800

    add voice sending

---
+++ app/src/main/java/com/tencent/qcloud/timchat/model/VoiceMessage.java
@@ -0,0 +1,95 @@
+package com.tencent.qcloud.timchat.model;
+
+import android.os.Environment;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+
+import com.tencent.TIMMessage;
+import com.tencent.TIMSoundElem;
+import com.tencent.TIMValueCallBack;
+import com.tencent.qcloud.timchat.MyApplication;
+import com.tencent.qcloud.timchat.R;
+import com.tencent.qcloud.timchat.adapters.ChatAdapter;
+import com.tencent.qcloud.timchat.utils.MediaUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 语音消息数据
+ */
+public class VoiceMessage extends Message {
+
+    private static final String TAG = "VoiceMessage";
+
+    public VoiceMessage(TIMMessage message){
+        this.message = message;
+    }
+
+
+    /**
+     * 语音消息构造方法
+     *
+     * @param duration 时长
+     * @param data 语音数据
+     */
+    public VoiceMessage(long duration,byte[] data){
+        message = new TIMMessage();
+        TIMSoundElem elem = new TIMSoundElem();
+        elem.setData(data);
+        elem.setDuration(duration);  //填写语音时长
+        message.addElement(elem);
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param viewHolder 界面样式
+     */
+    @Override
+    public void showMessage(ChatAdapter.ViewHolder viewHolder) {
+        TextView tv = new TextView(MyApplication.getContext());
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        tv.setTextColor(MyApplication.getContext().getResources().getColor(isSelf() ? R.color.white : R.color.black));
+        tv.setText(String.valueOf(((TIMSoundElem) message.getElement(0)).getDuration()) + "’");
+        getBubbleView(viewHolder).removeAllViews();
+        getBubbleView(viewHolder).addView(tv);
+        getBubbleView(viewHolder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VoiceMessage.this.playMp3();
+            }
+        });
+    }
+
+    public void playMp3() {
+        TIMSoundElem elem = (TIMSoundElem) message.getElement(0);
+
+        elem.getSound(new TIMValueCallBack<byte[]>() {
+            @Override
+            public void onError(int i, String s) {
+
+            }
+
+            @Override
+            public void onSuccess(byte[] bytes) {
+                try{
+                    File tempMp3 = File.createTempFile("kurchina", "mp3", Environment.getExternalStorageDirectory());
+                    tempMp3.deleteOnExit();
+                    FileOutputStream fos = new FileOutputStream(tempMp3);
+                    fos.write(bytes);
+                    fos.close();
+                    FileInputStream fis = new FileInputStream(tempMp3);
+                    MediaUtil.getInstance().play(fis);
+                }catch (IOException e){
+
+                }
+
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tencent/qcloud/timchat/utils/MediaUtil.java
@@ -0,0 +1,47 @@
+package com.tencent.qcloud.timchat.utils;
+
+import android.media.MediaPlayer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * 媒体播放工具
+ */
+public class MediaUtil {
+
+    private static final String TAG = "MediaUtil";
+
+    private MediaPlayer player;
+
+    private MediaUtil(){
+        player = new MediaPlayer();
+    }
+
+    private static MediaUtil instance = new MediaUtil();
+
+    public static MediaUtil getInstance(){
+        return instance;
+    }
+
+    public MediaPlayer getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(MediaPlayer player) {
+        this.player = player;
+    }
+
+    public void play(FileInputStream inputStream){
+        try{
+            player.reset();
+            player.setDataSource(inputStream.getFD());
+            player.prepare();
+            player.start();
+        }catch (IOException e){
+            LogUtils.e(TAG,"play error:"+e);
+        }
+
+
+    }
+}
