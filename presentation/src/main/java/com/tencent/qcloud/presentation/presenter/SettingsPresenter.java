9e4b484b8d65bb04c5db26600230afbe4e02048a
WillGuo <guozhuoxing@gmail.com>
2016-03-09 19:31:16 +0800

    change default new friend confirm

--- presentation/src/main/java/com/tencent/qcloud/presentation/presenter/SettingsPresenter.java
+++ presentation/src/main/java/com/tencent/qcloud/presentation/presenter/SettingsPresenter.java
@@ -1,8 +1,26 @@
 package com.tencent.qcloud.presentation.presenter;
 
+import com.tencent.TIMCallBack;
+import com.tencent.TIMFriendAllowType;
+import com.tencent.TIMFriendshipManager;
+
 /**
  * Created by admin on 16/3/9.
  */
 public class SettingsPresenter extends Presenter {
 
+    public void setDefaultAllowType(){
+        TIMFriendshipManager.getInstance().setAllowType(TIMFriendAllowType.TIM_FRIEND_NEED_CONFIRM, new TIMCallBack() {
+
+            @Override
+            public void onError(int arg0, String arg1) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onSuccess() {
+            }
+
+        });
+    }
 }
