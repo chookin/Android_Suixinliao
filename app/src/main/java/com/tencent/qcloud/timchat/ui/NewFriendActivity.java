2f1381a8c03e74763a682bf0f1132ae78e5252d0
WillGuo <guozhuoxing@gmail.com>
2016-03-16 10:56:03 +0800

    update profileActivity

--- app/src/main/java/com/tencent/qcloud/timchat/ui/NewFriendActivity.java
+++ app/src/main/java/com/tencent/qcloud/timchat/ui/NewFriendActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import com.tencent.TIMFriendFutureItem;
 import com.tencent.qcloud.presentation.presenter.GetFutureFriListPresenter;
 import com.tencent.qcloud.presentation.presenter.ResponseFriInvitePresenter;
+import com.tencent.qcloud.presentation.viewfeatures.AgreeNewFriend;
 import com.tencent.qcloud.presentation.viewfeatures.GetFutureFriListData;
 import com.tencent.qcloud.timchat.R;
 import com.tencent.qcloud.timchat.adapters.NewFriAdapter;
@@ -20,8 +21,8 @@ import java.util.List;
 /**
  * 查找添加新朋友
  */
-public class NewFriendActivity extends Activity implements AdapterView.OnItemClickListener ,GetFutureFriListData{
-    private String id;
+public class NewFriendActivity extends Activity implements AdapterView.OnItemClickListener, AgreeNewFriend, GetFutureFriListData {
+    private String mSelectId;
     private ListView mNewFriList;
     private NewFriAdapter mNewFriAdapter;
     private List<TIMFriendFutureItem> mNewFriListReuslt;
@@ -32,13 +33,13 @@ public class NewFriendActivity extends Activity implements AdapterView.OnItemCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newfriend);
-        mNewFriList = (ListView)findViewById(R.id.newfriend_list);
+        mNewFriList = (ListView) findViewById(R.id.newfriend_list);
         mNewFriList.setOnItemClickListener(this);
         mNewFriListReuslt = new ArrayList<TIMFriendFutureItem>();
-        mNewFriAdapter = new NewFriAdapter(this,mNewFriListReuslt,this);
+        mNewFriAdapter = new NewFriAdapter(this, mNewFriListReuslt, this);
         mNewFriList.setAdapter(mNewFriAdapter);
-        mGetFutureFriListPresenter = new GetFutureFriListPresenter(getBaseContext(),this);
-        mResponseFriInvitePresenter = new ResponseFriInvitePresenter(getBaseContext());
+        mGetFutureFriListPresenter = new GetFutureFriListPresenter(getBaseContext(), this);
+        mResponseFriInvitePresenter = new ResponseFriInvitePresenter(getBaseContext(), this);
         mGetFutureFriListPresenter.getFutureFriList();
         mNewFriList.setOnItemClickListener(this);
     }
@@ -56,13 +57,21 @@ public class NewFriendActivity extends Activity implements AdapterView.OnItemCli
 
     @Override
     public void showFutureFriListData(List<TIMFriendFutureItem> mFutureFriList) {
-        for(TIMFriendFutureItem newFri : mFutureFriList){
+        for (TIMFriendFutureItem newFri : mFutureFriList) {
             mNewFriListReuslt.add(newFri);
         }
         mNewFriAdapter.notifyDataSetChanged();
     }
 
-    public void confirmItemStatus(String id){
-        mResponseFriInvitePresenter.answerFriInvite(id,true);
+    public void confirmItemStatus(String id) {
+        mSelectId = id;
+        mResponseFriInvitePresenter.answerFriInvite(id, true);
+    }
+
+    @Override
+    public void jumpIntoProfileActicity() {
+        Intent intent = new Intent(NewFriendActivity.this, ProfileActivity.class);
+        intent.putExtra("identify", mSelectId);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/tencent/qcloud/timchat/ui/NewFriendDetailActivity.java
+++ app/src/main/java/com/tencent/qcloud/timchat/ui/NewFriendDetailActivity.java
@@ -6,38 +6,45 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.tencent.qcloud.presentation.presenter.ResponseFriInvitePresenter;
+import com.tencent.qcloud.presentation.viewfeatures.AgreeNewFriend;
 import com.tencent.qcloud.timchat.R;
 
 /**
  * 新朋友信息
  */
-public class NewFriendDetailActivity extends Activity implements View.OnClickListener {
+public class NewFriendDetailActivity extends Activity implements View.OnClickListener ,AgreeNewFriend{
 
-    private TextView mIdView,mBtnAgree, mBtnRefuse;
+    private TextView mIdView, mBtnAgree, mBtnRefuse;
     private ResponseFriInvitePresenter mResponseFriInvitePresenter;
     private String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newfri_detail);
-        mIdView = (TextView)findViewById(R.id.newfri_info_id);
-        mBtnAgree = (TextView)findViewById(R.id.agree_btn);
-        mBtnRefuse = (TextView)findViewById(R.id.refuse_btn);
+        mIdView = (TextView) findViewById(R.id.newfri_info_id);
+        mBtnAgree = (TextView) findViewById(R.id.agree_btn);
+        mBtnRefuse = (TextView) findViewById(R.id.refuse_btn);
         mBtnAgree.setOnClickListener(this);
         mBtnRefuse.setOnClickListener(this);
-        mResponseFriInvitePresenter = new ResponseFriInvitePresenter(this);
+        mResponseFriInvitePresenter = new ResponseFriInvitePresenter(this,this);
         id = getIntent().getStringExtra("id");
         mIdView.setText(id);
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId()==R.id.agree_btn){
-            mResponseFriInvitePresenter.answerFriInvite(id,true);
+        if (view.getId() == R.id.agree_btn) {
+            mResponseFriInvitePresenter.answerFriInvite(id, true);
         }
-        if(view.getId()==R.id.refuse_btn){
-            mResponseFriInvitePresenter.answerFriInvite(id,false);
+        if (view.getId() == R.id.refuse_btn) {
+            mResponseFriInvitePresenter.answerFriInvite(id, false);
 
         }
+    }
+
+    @Override
+    public void jumpIntoProfileActicity() {
+
     }
 }
