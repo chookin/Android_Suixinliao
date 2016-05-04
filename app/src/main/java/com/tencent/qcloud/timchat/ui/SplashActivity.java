93124369b111f32902fbcb37b076a131df2c33c7
jasonxiao <jasonxiao@tencent.com>
2016-05-04 11:17:33 +0800

    fix login error

--- app/src/main/java/com/tencent/qcloud/timchat/ui/SplashActivity.java
+++ app/src/main/java/com/tencent/qcloud/timchat/ui/SplashActivity.java
@@ -125,13 +125,15 @@ public class SplashActivity extends FragmentActivity implements SplashView,TIMCa
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (LOGIN_RESULT_CODE == requestCode) {
+            if (resultCode == RESULT_CANCELED){
+                finish();
+                return;
+            }
             if (0 == TLSService.getInstance().getLastErrno()){
                 String id = TLSService.getInstance().getLastUserIdentifier();
                 UserInfo.getInstance().setId(id);
                 UserInfo.getInstance().setUserSig(TLSService.getInstance().getUserSig(id));
                 navToHome();
-            }else if (resultCode == RESULT_CANCELED){
-                finish();
             }
         }
     }
